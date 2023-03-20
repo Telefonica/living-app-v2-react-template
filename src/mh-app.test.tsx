40027b5e8fede0d03921fff0c93ea04789113a33
Eduardo Parra <eduardo.parramazuecos@telefonica.com>
2023-03-20 10:28:10 +0100

    add eslint rules

--- src/mh-app.test.tsx
+++ src/mh-app.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import MhApp from './mh-app';
 
 describe('<MhApp />', () => {
-  test('render: match snapshot', () => {
+  it('render: match snapshot', () => {
     const { asFragment } = render(<MhApp />);
     expect(asFragment()).toMatchSnapshot();
   });

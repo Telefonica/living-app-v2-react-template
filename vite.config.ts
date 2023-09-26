3374f90c6cf1186e5e151061937839752570d005
Eduardo Parra <eduardo.parramazuecos@telefonica.com>
2023-09-26 14:31:56 +0200

    change babel by swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { loadEnv } from 'vite';
 
 /* Plugins */
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { createHtmlPlugin as html } from 'vite-plugin-html';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';

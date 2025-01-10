ea1a81c1d39144e99a57d91a50dbacbecfbe8dcf
Eduardo Parra <eduardo.parramazuecos@telefonica.com>
2025-01-10 12:48:14 +0100

    pr comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { MhApp } from 'mh-app';
 import { StbApp } from 'stb-app';
 
 import { name } from '../package.json';
@@ -10,14 +9,12 @@ import './index.css';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
-const App = laSdk.channel === 'movistar-home' ? MhApp : StbApp;
-
 window.laSdk
   .ready({ laName: name.replace('living-app-v2-', '') })
   .then(() => {
     root.render(
       <StrictMode>
-        <App />
+        <StbApp />
       </StrictMode>,
     );
 
--- src/mh-app.tsx
+++
@@ -1,5 +0,0 @@
-import type { JSX } from 'react';
-
-export const MhApp = (): JSX.Element => {
-  return <div />;
-};

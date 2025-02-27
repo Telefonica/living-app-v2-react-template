990104976c38d63fba8cf7645a69afb028cde388
Patricia Roman Sanchez <patricia.romansanchez@telefonica.com>
2025-02-27 12:18:43 +0100

    Include .qa files

---
+++ .qa/reserve.sh
@@ -0,0 +1,6 @@
+python .venv/lib/python3.11/site-packages/laqacommons/tools/management/management.py \
+    --reserve \
+    --phone "+34XXXXXXXXX" \
+    --user-id user_ci_0 \
+    --environment dev \
+    --admin_code XXXXXXXXXXXXXX
